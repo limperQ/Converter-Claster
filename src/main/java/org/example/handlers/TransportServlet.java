4baf9e15a0a3a693717196b009644bdb724a9e25
Eugene Sukharev <r0bari@yandex.ru>
2020-05-06 09:46:21 +0300

    Создал TransportServlet, добавил порты для серверов в main.props

---
+++ src/main/java/org/example/handlers/TransportServlet.java
@@ -0,0 +1,51 @@
+package org.example.handlers;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.example.Main;
+import org.example.model.Answer;
+import org.example.model.Item;
+import org.example.model.Request;
+import org.example.utils.Common;
+import org.example.utils.PropertyManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class TransportServlet extends HttpServlet {
+    private static int counter = 0;
+    private static int serverCounter = 3;
+    private static Logger log = LoggerFactory.getLogger(TransportServlet.class.getSimpleName());
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String redirectingPath = req.getContextPath();
+        switch(counter % serverCounter) {
+            case (0): {
+                redirectingPath += PropertyManager.getPropertyAsInteger("serverTransport.port", 8026).toString();
+            }
+            break;
+            case (1): {
+                redirectingPath += PropertyManager.getPropertyAsInteger("serverConverter1.port", 8027).toString();
+            }
+            break;
+            case (2): {
+                redirectingPath += PropertyManager.getPropertyAsInteger("serverConverter2.port", 8028).toString();
+            }
+            break;
+        }
+        redirectingPath += "/convert";
+        resp.sendRedirect(redirectingPath);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
