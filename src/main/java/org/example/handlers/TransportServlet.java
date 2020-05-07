724b7ca8965a2c7190f277be6b5c3afa0009d41c
Eugene Sukharev <r0bari@yandex.ru>
2020-05-07 17:53:06 +0300

    TransportServlet, переадресация GET-запросов работает. POST-запросы пока тоже переадресуются на doGet()

--- src/main/java/org/example/handlers/TransportServlet.java
+++ src/main/java/org/example/handlers/TransportServlet.java
@@ -16,8 +16,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 public class TransportServlet extends HttpServlet {
     private static int counter = 0;
@@ -25,27 +23,29 @@ public class TransportServlet extends HttpServlet {
     private static Logger log = LoggerFactory.getLogger(TransportServlet.class.getSimpleName());
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String redirectingPath = req.getContextPath();
+        String redirectingPath = "http://localhost:";
+
         switch(counter % serverCounter) {
-            case (0): {
-                redirectingPath += PropertyManager.getPropertyAsInteger("serverTransport.port", 8026).toString();
-            }
-            break;
-            case (1): {
-                redirectingPath += PropertyManager.getPropertyAsInteger("serverConverter1.port", 8027).toString();
-            }
-            break;
-            case (2): {
-                redirectingPath += PropertyManager.getPropertyAsInteger("serverConverter2.port", 8028).toString();
-            }
-            break;
+            case (0): redirectingPath += PropertyManager.getPropertyAsInteger("serverTransport.port", 8026).toString(); break;
+            case (1): redirectingPath += PropertyManager.getPropertyAsInteger("serverConverter1.port", 8027).toString(); break;
+            case (2): redirectingPath += PropertyManager.getPropertyAsInteger("serverConverter2.port", 8028).toString(); break;
         }
+        //  TODO: переадресует на doGET, нужно на doPost
         redirectingPath += "/convert";
         resp.sendRedirect(redirectingPath);
+        ++serverCounter;
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        String redirectingPath = "http://localhost:";
+        switch(counter % serverCounter) {
+            case (0): redirectingPath += PropertyManager.getPropertyAsInteger("serverTransport.port", 8026).toString(); break;
+            case (1): redirectingPath += PropertyManager.getPropertyAsInteger("serverConverter1.port", 8027).toString(); break;
+            case (2): redirectingPath += PropertyManager.getPropertyAsInteger("serverConverter2.port", 8028).toString(); break;
+        }
+        redirectingPath += "/convert";
+        resp.sendRedirect(redirectingPath);
+        ++serverCounter;
     }
 }
